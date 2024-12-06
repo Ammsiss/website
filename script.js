65ebdfa23c0cb29623ed1596ee06744eca658299
Ammsiss <j7nj1.t41@gmail.com>
2024-12-06 19:55:22 -0400

    init commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function menu() 
+{
+    const menu = document.querySelector(".item-links");
+    const icon = document.querySelector(".expand-icon");
+    menu.classList.toggle("display");
+    icon.classList.toggle("open");
+}
